2f507f6ff37ffb541e714a9a6f08f9138f8714af
Franciane Lark <franciane.lark019@gmail.com>
2023-05-10 21:43:21 -0400

    update projeto

--- Aula02/script.js
+++ Aula02/script.js
@@ -1,3 +1,11 @@
+/*
+função getValue
+a variável recebe o valor de name e o título h2 recebe como resultado: "Bem vindo, Franciane"
+
+função removeValue
+o título name para vazio
+o resultado de h2 será vazio
+*/
 function getValue(){
   var value = document.getElementById("name").value;
   document.getElementById("result").innerHTML = "Bem vindo, " + value + "!";
